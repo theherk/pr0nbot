7b003f19d693310c02a6326eccacf59f045344bf
Pranav <pranav.89@gmail.com>
2017-08-17 15:08:53 -0700

    Hit reddit api and get top posts from sub-reddits

--- main.go
+++ main.go
@@ -2,14 +2,28 @@ package main
 
 import (
 	"fmt"
-	"os"
+	"strings"
 
-	"github.com/theherk/pr0nbot/cmd"
+	"github.com/turnage/graw/reddit"
 )
 
+type announcer struct{}
+
 func main() {
-	if err := cmd.RootCmd.Execute(); err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	bot, err := reddit.NewBotFromAgentFile("credentials.txt", 0)
+	harvest, err := bot.Listing("/r/pics", "")
+	if err != nil {
+		fmt.Println("Failed to fetch /r/golang: ", err)
+		return
 	}
+
+	for _, post := range harvest.Posts[:5] {
+		if strings.HasSuffix(post.URL, ".jpg") {
+			fmt.Printf("[%s] posted [%s]\n", post.Title, post.URL)
+		}
+	}
+	// if err := cmd.RootCmd.Execute(); err != nil {
+	// 	fmt.Println(err)
+	// 	os.Exit(1)
+	// }
 }
