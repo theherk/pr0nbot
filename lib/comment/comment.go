7fd7b292d69a1e818d270188f7592f069a8d8b2b
Adam Sherwood <theherk@gmail.com>
2017-08-17 16:48:58 -0700

    Add function to reply

--- lib/comment/comment.go
+++ lib/comment/comment.go
@@ -1,2 +1,17 @@
 // Package comment post comments to reddit.
 package comment
+
+import (
+	"fmt"
+
+	"github.com/turnage/graw/reddit"
+)
+
+const tmpl = "__This image contains prawn__.\n\nWould somebody please think of " +
+	"the children? Posting images containing prawn should met with scorn and " +
+	"tagged appropriately. shame, shame, shame... _bong_. %s, you must confess."
+
+// Do will reply to a reddit comment.
+func Do(b reddit.Bot, p reddit.Post) error {
+	return b.Reply(p.Name, fmt.Sprintf(tmpl, p.Author))
+}
